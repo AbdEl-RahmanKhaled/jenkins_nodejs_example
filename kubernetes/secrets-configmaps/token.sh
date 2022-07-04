4b5e149a1424b12197e2952b19b203ecbfbcd491
AbdEl-RahmanKhaled <a.kha.anw@gmail.com>
2022-07-04 17:46:20 +0200

    update Jenkinsfile

--- kubernetes/secrets-configmaps/token.sh
+++ kubernetes/secrets-configmaps/token.sh
@@ -1,4 +1,15 @@
- cat << EOF | base64 | xargs -E . ./kubernetes/secrets-configmaps/nexus-sec.sh
+cat << EOF | base64
+{
+  "auths": {
+    "10.107.185.142:8082": {
+      "email": $USERNAME,
+      "auth": echo -n $PASS | base64
+    }
+  }
+}
+EOF
+
+cat << EOF | base64 | xargs -E . ./kubernetes/secrets-configmaps/nexus-sec.sh
 {
   "auths": {
     "10.107.185.142:8082": {
