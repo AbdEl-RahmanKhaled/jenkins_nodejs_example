9146acaa209edf34b4a7c313a12bc37c5b482a39
AbdEl-RahmanKhaled <a.kha.anw@gmail.com>
2022-07-04 19:44:56 +0200

    create kubernetes files

--- kubernetes/secrets-configmaps/mysql-cred.sh
+++ kubernetes/secrets-configmaps/mysql-cred.sh
@@ -7,7 +7,7 @@ metadata:
 data:
   MYSQL_DATABASE: `echo -n mydb | base64 -w0`
   MYSQL_USER: `echo -n $USERNAME | base64 -w0`
-  MYSQL_PASSWORD:`echo -n $PASS | base64 -w0`
+  MYSQL_PASSWORD: `echo -n $PASS | base64 -w0`
 EOF
 
 cat << EOF | kubectl apply -f-
@@ -19,5 +19,5 @@ metadata:
 data:
   MYSQL_DATABASE: `echo -n mydb | base64 -w0`
   MYSQL_USER: `echo -n $USERNAME | base64 -w0`
-  MYSQL_PASSWORD:`echo -n $PASS | base64 -w0`
+  MYSQL_PASSWORD: `echo -n $PASS | base64 -w0`
 EOF
